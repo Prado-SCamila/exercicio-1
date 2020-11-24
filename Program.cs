2a20905ed1a1fb870ff410b9251addf0d142372c
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-24 09:36:57 -0300

    Fim

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Idade para votar");
+
+          
+            int idade;
+
+            Console.WriteLine("Digite seu ano de nascimento");
+            int an = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu ano atual");
+            int aa = int.Parse(Console.ReadLine());
+
+            idade = aa- an;
+
+            Console.WriteLine(idade);
+
+            if(idade<18){
+                Console.WriteLine("Você não pode votar ainda.");
+
+            }else{
+                Console.WriteLine("Você pode votar.");
+            }
+        }
+    }
+}
